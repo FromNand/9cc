b192dd73cc39cb804664c006a40494152c215de9
FromNand <celestial.folt@gmail.com>
2020-09-15 18:57:16 +0900

    複数の項の足し算と引き算をコンパイルできるようになった

--- 9cc.c
+++ 9cc.c
@@ -3,14 +3,32 @@
 
 int main(int argc, char **argv){
 	if(argc != 2){
-		fprintf(stderr, "引数の個数が正しくありません。\n");
+		fprintf(stderr, "引数の個数が正しくありません\n");
 		return 1;
 	}
 
 	printf(".intel_syntax noprefix\n");
 	printf(".globl main\n\n");
 	printf("main:\n");
-	printf("\tmov rax, %d\n", atoi(argv[1]));
+
+	char *p = argv[1];
+
+	printf("\tmov rax, %ld\n", strtol(p, &p, 10));
+
+	while(*p){
+		if(*p == '+'){
+			p++;
+			printf("\tadd rax, %ld\n", strtol(p, &p, 10));
+		}
+		else if(*p == '-'){
+			p++;
+			printf("\tsub rax, %ld\n", strtol(p, &p, 10));
+		}
+		else{
+			printf("コンパイル不能な文字'%c'が発見されました\n", *p);
+			return 2;
+		}
+	}
 	printf("\tret\n");
 
 	return 0;
